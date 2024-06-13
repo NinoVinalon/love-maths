8f6fb140a283b9f2fe9dc1badacf620521c215e2
Nino Alexander Vinalon <navinalon@gmail.com>
2024-06-13 21:32:17 +0000

    Add code to increment score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,8 +36,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert ("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awww... you answered ${userAnswer}.The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGAme(calculatedAnswer[1]);
 
@@ -58,11 +60,13 @@ function calculateCorrectAnswer() {
 }
 
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
